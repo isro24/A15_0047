c4202104d3aa2547fe9eb825cddcda5e8f908c03
isro24 <alamat_email_anda@example.com>
2025-01-19 19:35:59 +0700

    Menambah KandangRepository pada repository

---
+++ app/src/main/java/com/example/uas/repository/KandangRepository.kt
@@ -0,0 +1,57 @@
+package com.example.uas.repository
+
+import com.example.uas.model.AllHewanResponse
+import com.example.uas.model.AllKandangResponse
+import com.example.uas.model.Hewan
+import com.example.uas.model.HewanDetailResponse
+import com.example.uas.model.Kandang
+import com.example.uas.model.KandangDetailResponse
+import com.example.uas.service_api.HewanService
+import com.example.uas.service_api.KandangService
+import java.io.IOException
+
+interface KandangRepository{
+    suspend fun insertKandang(kandang: Kandang)
+
+    suspend fun getKandang(): AllKandangResponse
+
+    suspend fun updateKandang(idKandang: String, kandang: Kandang)
+
+    suspend fun deleteKandang(idKandang: String)
+
+    suspend fun getKandangById(idKandang: String): KandangDetailResponse
+}
+
+class NetworkKandangRepository(
+    private val kandangApiService: KandangService
+): KandangRepository{
+    override suspend fun insertKandang(kandang: Kandang) {
+        kandangApiService.insertKandang(kandang)
+    }
+
+    override suspend fun updateKandang(idKandang: String, kandang: Kandang) {
+        kandangApiService.updateKandang(idKandang, kandang)
+    }
+
+    override suspend fun deleteKandang(idKandang: String) {
+        try {
+            val response = kandangApiService.deleteKandang(idKandang)
+            if (!response.isSuccessful){
+                throw IOException("Failed to delete Kandang. HTTP Status code: " +
+                        "${response.code()}")
+            }else{
+                response.message()
+                println(response.message())
+            }
+        }catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getKandang(): AllKandangResponse =
+        kandangApiService.getAllKandang()
+
+    override suspend fun getKandangById(idKandang: String): KandangDetailResponse {
+        return kandangApiService.getKandangById(idKandang)
+    }
+}
