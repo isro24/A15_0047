ea0914c3d53c47bc4b06e74b440903b4c2abd444
isro24 <alamat_email_anda@example.com>
2025-01-19 17:59:23 +0700

    Menambah data class Kandang pada model

---
+++ app/src/main/java/com/example/uas/model/Kandang.kt
@@ -0,0 +1,28 @@
+package com.example.uas.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AllKandangResponse (
+    val status : Boolean,
+    val message : String,
+    val data: List<Kandang>
+)
+
+@Serializable
+data class KandangDetailResponse(
+    val status : Boolean,
+    val message : String,
+    val data: Kandang
+)
+@Serializable
+data class Kandang (
+    @SerialName("id_kandang")
+    val idKandang: Int,
+    @SerialName("id_hewan")
+    val idHewan: Int,
+    val kapasitas: String,
+    val lokasi: String,
+
+)
