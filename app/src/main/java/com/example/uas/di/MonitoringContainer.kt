9f21b46295dce43ab97c66e93a0381894db12201
isro24 <alamat_email_anda@example.com>
2025-01-19 20:22:56 +0700

    Menambah MonitoringContainer pada dependenciesinjection

---
+++ app/src/main/java/com/example/uas/di/MonitoringContainer.kt
@@ -0,0 +1,32 @@
+package com.example.uas.di
+
+import com.example.uas.repository.KandangRepository
+import com.example.uas.repository.MonitoringRepository
+import com.example.uas.repository.NetworkKandangRepository
+import com.example.uas.repository.NetworkMonitoringRepository
+import com.example.uas.service_api.KandangService
+import com.example.uas.service_api.MonitoringService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainerMonitoring{
+    val monitoringRepository: MonitoringRepository
+}
+
+class MonitoringContainer: AppContainerMonitoring{
+    private val baseUrl= "http://10.0.2.2:3000/api/monitoring/"
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val monitoringService: MonitoringService by lazy {
+        retrofit.create(MonitoringService::class.java)
+    }
+
+    override val monitoringRepository: MonitoringRepository by lazy {
+        NetworkMonitoringRepository(monitoringService)
+    }
+}
