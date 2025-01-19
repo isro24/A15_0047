8572571f9f2d6bda65334e2de04055366a8d76b6
isro24 <alamat_email_anda@example.com>
2025-01-19 20:13:50 +0700

    Menambah KandangContainer pada dependenciesinjection

--- app/src/main/java/com/example/uas/di/KandangContainer.kt
+++ app/src/main/java/com/example/uas/di/KandangContainer.kt
@@ -1,29 +1,32 @@
 package com.example.uas.di
 
 import com.example.uas.repository.HewanRepository
+import com.example.uas.repository.KandangRepository
 import com.example.uas.repository.NetworkHewanRepository
+import com.example.uas.repository.NetworkKandangRepository
 import com.example.uas.service_api.HewanService
+import com.example.uas.service_api.KandangService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
-interface AppContainerHewan{
-    val hewanRepository: HewanRepository
+interface AppContainerKandang{
+    val kandangRepository: KandangRepository
 }
 
-class HewanContainer: AppContainerHewan{
-    private val baseUrl= "http://10.0.2.2:3000/api/hewan/"
+class KandangContainer: AppContainerKandang{
+    private val baseUrl= "http://10.0.2.2:3000/api/kandang/"
     private val json = Json { ignoreUnknownKeys = true }
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(baseUrl).build()
 
-    private val hewanService: HewanService by lazy {
-        retrofit.create(HewanService::class.java)
+    private val kandangService: KandangService by lazy {
+        retrofit.create(KandangService::class.java)
     }
 
-    override val hewanRepository: HewanRepository by lazy {
-        NetworkHewanRepository(hewanService)
+    override val kandangRepository: KandangRepository by lazy {
+        NetworkKandangRepository(kandangService)
     }
 }
