638331386c4d028eb9b59629f9975f2b2c154797
isro24 <alamat_email_anda@example.com>
2025-01-20 20:20:46 +0700

    Menambah initializer untuk DetailViewModel hewan pada PenyediaViewModel

--- app/src/main/java/com/example/uas/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/uas/ui/viewmodel/PenyediaViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.uas.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.uas.ZooMonitoringApplications
+import com.example.uas.ui.viewmodel.hewan.DetailViewModel
 import com.example.uas.ui.viewmodel.hewan.HomeViewModel
 import com.example.uas.ui.viewmodel.hewan.InsertViewModel
 
@@ -12,6 +14,7 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiZoo().containerHewan.hewanRepository) }
         initializer { InsertViewModel(aplikasiZoo().containerHewan.hewanRepository) }
+        initializer { DetailViewModel(createSavedStateHandle(),aplikasiZoo().containerHewan.hewanRepository) }
     }
 }
 
