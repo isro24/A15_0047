2ea407bfb5fedd163e5db1b351f707dfe1aa880c
isro24 <alamat_email_anda@example.com>
2025-01-20 20:54:00 +0700

    Menambah UpdateView untuk hewan di package View  Hewan

---
+++ app/src/main/java/com/example/uas/ui/view/hewan/UpdateView.kt
@@ -0,0 +1,61 @@
+package com.example.uas.ui.view.hewan
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uas.ui.customwidget.CustomeTopAppBar
+import com.example.uas.ui.navigation.DestinasiNavigasi
+import com.example.uas.ui.viewmodel.PenyediaViewModel
+import com.example.uas.ui.viewmodel.hewan.UpdateViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+object DestinasiUpdateHewan : DestinasiNavigasi {
+    override val route = "update"
+    override val titleRes = "Edit Hewan"
+    const val IDHEWAN = "id_hewan"
+    val routeWithArgs = "$route/{$IDHEWAN}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateViewHewan(
+    NavigateBack: () -> Unit,
+    onNavigate:()-> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold (
+        modifier = modifier,
+        topBar = {
+            CustomeTopAppBar(
+                title = DestinasiUpdateHewan.titleRes,
+                canNavigateBack = true,
+                navigateUp = NavigateBack,
+            )
+        }
+    ){ padding ->
+        EntryBody(
+            modifier = Modifier.padding(padding),
+            onSiswaValueChange = viewModel::updateInsertHwnState,
+            insertUiState = viewModel.updateUIState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateData()
+                    delay(600)
+                    withContext(Dispatchers.Main){
+                        onNavigate()
+                    }
+                }
+            }
+        )
+    }
+}
