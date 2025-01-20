68760c294ca95c9848803d0bc1dd6c686259ab76
isro24 <alamat_email_anda@example.com>
2025-01-20 15:33:33 +0700

    Menambah HomeViewModel untuk hewan di package ViewModel Hewan

---
+++ app/src/main/java/com/example/uas/ui/viewmodel/hewan/HomeViewModel.kt
@@ -0,0 +1,51 @@
+package com.example.uas.ui.viewmodel.hewan
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uas.model.Hewan
+import com.example.uas.repository.HewanRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed class HomeUiState {
+    data class Success(val hewan : List<Hewan>) : HomeUiState()
+    object Error : HomeUiState()
+    object Loading : HomeUiState()
+}
+
+class HomeViewModel(private val hwn: HewanRepository) : ViewModel(){
+    var hwnUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getHwn()
+    }
+
+    fun getHwn(){
+        viewModelScope.launch {
+            hwnUiState = HomeUiState.Loading
+            hwnUiState = try {
+                HomeUiState.Success(hwn.getHewan().data)
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: IOException) {
+                HomeUiState.Error
+            }
+        }
+    }
+
+    fun deleteHwn(idHewan: String) {
+        viewModelScope.launch {
+            try {
+                hwn.deleteHewan(idHewan)
+            } catch (e: IOException){
+                HomeUiState.Error
+            } catch (e: IOException){
+                HomeUiState.Error
+            }
+        }
+    }
+}
