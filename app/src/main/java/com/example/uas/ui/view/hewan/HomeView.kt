12249b0a02c51b5afce17b83c731580af539577d
isro24 <alamat_email_anda@example.com>
2025-01-20 16:45:46 +0700

    Menambah string name pada strings.xml dan HomeView untuk hewan di package View Hewan

---
+++ app/src/main/java/com/example/uas/ui/view/hewan/HomeView.kt
@@ -0,0 +1,201 @@
+package com.example.uas.ui.view.hewan
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uas.R
+import com.example.uas.model.Hewan
+import com.example.uas.ui.navigation.DestinasiNavigasi
+import com.example.uas.ui.viewmodel.PenyediaViewModel
+import com.example.uas.ui.viewmodel.hewan.HomeUiState
+import com.example.uas.ui.viewmodel.hewan.HomeViewModel
+
+object DestinasiHomeHewan : DestinasiNavigasi{
+    override val route = "homehewan"
+    override val titleRes = "Home Hewan"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreenHewan(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    var showDeleteDialog by remember { mutableStateOf(false) }
+
+    var selectedHewan: Hewan? by remember { mutableStateOf(null) }
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomeTopAppBar(
+                title = DestinasiHomeHewan.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getHwn()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ){
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Hewan"
+                )
+            }
+        }
+    ){innerPadding ->
+        HomeStatusHewan(
+            homeUiState = viewModel.hwnUiState,
+            retryAction = {viewModel.getHwn()},
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick
+        )
+    }
+}
+
+@Composable
+fun HomeStatusHewan(
+    homeUiState: HomeUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit
+){
+    when (homeUiState){
+        is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is HomeUiState.Success ->
+            if (homeUiState.hewan.isEmpty()){
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                    Text(text = "Tidak ada data Hewan")
+                }
+            }else {
+                HwnLayout(
+                    hewan = homeUiState.hewan,
+                    modifier=modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.idHewan.toString())
+                    }
+                )
+            }
+        is HomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier
+            .size(100.dp)
+            .padding(40.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(painter = painterResource(id = R.drawable.connection_error), contentDescription = ""
+        )
+        Text(text = stringResource(id = R.string.loading_failed),
+            modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun HwnLayout(
+    hewan: List<Hewan>,
+    modifier: Modifier= Modifier,
+    onDetailClick: (Hewan) -> Unit,
+){
+    LazyColumn (
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        items(hewan) { hewan ->
+            MhsCard(
+                hewan = hewan,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(hewan) },
+            )
+        }
+    }
+}
+
+@Composable
+fun MhsCard(
+    hewan: Hewan,
+    modifier: Modifier = Modifier,
+){
+    Card (
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ){
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ){
+            Text(
+                text = hewan.namaHewan,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = hewan.tipePakan,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}
