533b5dbe4f06ffc8ba2c7d856f11c5855fe30b7f
isro24 <alamat_email_anda@example.com>
2025-01-21 12:48:12 +0700

    Menambah HomeViewModel untuk petugas di package ViewModel Petugas dan menghapus fun delete pada HomeViewModel Hewan

--- app/src/main/java/com/example/uas/ui/viewmodel/petugas/HomeViewModel.kt
+++ app/src/main/java/com/example/uas/ui/viewmodel/petugas/HomeViewModel.kt
@@ -1,34 +1,34 @@
-package com.example.uas.ui.viewmodel.hewan
+package com.example.uas.ui.viewmodel.petugas
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.uas.model.Hewan
-import com.example.uas.repository.HewanRepository
+import com.example.uas.model.Petugas
+import com.example.uas.repository.PetugasRepository
 import kotlinx.coroutines.launch
 import java.io.IOException
 
 sealed class HomeUiState {
-    data class Success(val hewan : List<Hewan>) : HomeUiState()
+    data class Success(val petugas : List<Petugas>) : HomeUiState()
     object Error : HomeUiState()
     object Loading : HomeUiState()
 }
 
-class HomeViewModel(private val hwn: HewanRepository) : ViewModel(){
-    var hwnUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+class HomeViewModel(private val ptg: PetugasRepository) : ViewModel(){
+    var ptgUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
         private set
 
     init {
-        getHwn()
+        getPtg()
     }
 
-    fun getHwn(){
+    fun getPtg(){
         viewModelScope.launch {
-            hwnUiState = HomeUiState.Loading
-            hwnUiState = try {
-                HomeUiState.Success(hwn.getHewan().data)
+            ptgUiState = HomeUiState.Loading
+            ptgUiState = try {
+                HomeUiState.Success(ptg.getPetugas().data)
             } catch (e: IOException) {
                 HomeUiState.Error
             } catch (e: IOException) {
