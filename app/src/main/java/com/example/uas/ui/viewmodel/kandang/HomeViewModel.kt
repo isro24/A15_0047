a370bdad11c6c77568d90ed67cd199e3e37c822e
isro24 <alamat_email_anda@example.com>
2025-01-22 00:48:14 +0700

    Menambah HomeViewModel untuk kandang di package ViewModel Kandang

---
+++ app/src/main/java/com/example/uas/ui/viewmodel/kandang/HomeViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.uas.ui.viewmodel.kandang
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uas.model.Kandang
+import com.example.uas.repository.KandangRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed class HomeUiStateKandang {
+    data class Success(val kandang : List<Kandang>) : HomeUiStateKandang()
+    object Error : HomeUiStateKandang()
+    object Loading : HomeUiStateKandang()
+}
+
+class HomeViewModelKandang(private val knd: KandangRepository) : ViewModel(){
+    var kndUiState: HomeUiStateKandang by mutableStateOf(HomeUiStateKandang.Loading)
+        private set
+
+    init {
+        getKnd()
+    }
+
+    fun getKnd(){
+        viewModelScope.launch {
+            kndUiState = HomeUiStateKandang.Loading
+            kndUiState = try {
+                HomeUiStateKandang.Success(knd.getKandang().data)
+            } catch (e: IOException) {
+                HomeUiStateKandang.Error
+            } catch (e: IOException) {
+                HomeUiStateKandang.Error
+            }
+        }
+    }
+}
