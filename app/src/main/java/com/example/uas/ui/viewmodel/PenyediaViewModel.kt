b219aefca13db059672fd344e585fc68fa59f3a2
isro24 <alamat_email_anda@example.com>
2025-01-24 20:56:50 +0700

    Menambah initializer ViewModel untuk Kandang pada PenyediaViewModel

--- app/src/main/java/com/example/uas/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/uas/ui/viewmodel/PenyediaViewModel.kt
@@ -10,6 +10,10 @@ import com.example.uas.ui.viewmodel.hewan.DetailViewModel
 import com.example.uas.ui.viewmodel.hewan.HomeViewModel
 import com.example.uas.ui.viewmodel.hewan.InsertViewModel
 import com.example.uas.ui.viewmodel.hewan.UpdateViewModel
+import com.example.uas.ui.viewmodel.kandang.DetailViewModelKandang
+import com.example.uas.ui.viewmodel.kandang.HomeViewModelKandang
+import com.example.uas.ui.viewmodel.kandang.InsertViewModelKandang
+import com.example.uas.ui.viewmodel.kandang.UpdateViewModelKandang
 import com.example.uas.ui.viewmodel.petugas.DetailViewModelPetugas
 import com.example.uas.ui.viewmodel.petugas.HomeViewModelPetugas
 import com.example.uas.ui.viewmodel.petugas.InsertViewModelPetugas
@@ -26,6 +30,11 @@ object PenyediaViewModel {
         initializer { InsertViewModelPetugas(aplikasiZoo().containerPetugas.petugasRepository) }
         initializer { DetailViewModelPetugas(createSavedStateHandle(),aplikasiZoo().containerPetugas.petugasRepository) }
         initializer { UpdateViewModelPetugas(createSavedStateHandle(),aplikasiZoo().containerPetugas.petugasRepository) }
+
+        initializer { HomeViewModelKandang(aplikasiZoo().containerKandang.kandangRepository) }
+        initializer { InsertViewModelKandang(aplikasiZoo().containerKandang.kandangRepository) }
+        initializer { DetailViewModelKandang(createSavedStateHandle(),aplikasiZoo().containerKandang.kandangRepository) }
+        initializer { UpdateViewModelKandang(createSavedStateHandle(),aplikasiZoo().containerKandang.kandangRepository) }
     }
 }
 
