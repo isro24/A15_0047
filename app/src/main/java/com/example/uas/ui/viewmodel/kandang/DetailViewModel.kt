67c84f6d8edc420a42b688f77c1150399f31879c
isro24 <alamat_email_anda@example.com>
2025-01-24 20:21:35 +0700

    Menambah DetailViewModel untuk kandang di package ViewModel Kandang

---
+++ app/src/main/java/com/example/uas/ui/viewmodel/kandang/DetailViewModel.kt
@@ -0,0 +1,85 @@
+package com.example.uas.ui.viewmodel.kandang
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uas.model.Kandang
+import com.example.uas.repository.KandangRepository
+import com.example.uas.ui.view.kandang.DestinasiDetailKandang
+import kotlinx.coroutines.launch
+
+class DetailViewModelKandang(
+    savedStateHandle: SavedStateHandle,
+    private val kandangRepository: KandangRepository
+) : ViewModel() {
+    private val idKandang: String = checkNotNull(savedStateHandle[DestinasiDetailKandang.IDKANDANG])
+
+    var detailUiStateKandang: DetailUiStateKandang by mutableStateOf(DetailUiStateKandang())
+        private set
+
+    init {
+        getKandangById()
+    }
+
+    fun getKandangById() {
+        viewModelScope.launch {
+            detailUiStateKandang = DetailUiStateKandang(isLoading = true)
+            try {
+                val result = kandangRepository.getKandangById(idKandang).data
+                detailUiStateKandang = DetailUiStateKandang(
+                    detailUiEventKandang = result.toDetailUiEventKandang(),
+                    isLoading = false
+                )
+            } catch (e: Exception) {
+                detailUiStateKandang = DetailUiStateKandang(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = e.message ?: "Unknown"
+                )
+            }
+        }
+    }
+
+    fun deleteKnd() {
+        viewModelScope.launch {
+            detailUiStateKandang = DetailUiStateKandang(isLoading = true)
+            try {
+                kandangRepository.deleteKandang(idKandang)
+
+                detailUiStateKandang = DetailUiStateKandang(isLoading = false)
+            } catch (e: Exception) {
+                detailUiStateKandang = DetailUiStateKandang(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = e.message ?: "Unknown Error"
+                )
+            }
+        }
+    }
+}
+
+
+data class DetailUiStateKandang(
+    val detailUiEventKandang: InsertUiEventKandang = InsertUiEventKandang(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEventKandang == InsertUiEventKandang()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEventKandang != InsertUiEventKandang()
+}
+
+fun Kandang.toDetailUiEventKandang(): InsertUiEventKandang {
+    return InsertUiEventKandang(
+        idKandang = idKandang,
+        idHewan =  idHewan,
+        kapasitas = kapasitas,
+        lokasi = lokasi
+    )
+}
