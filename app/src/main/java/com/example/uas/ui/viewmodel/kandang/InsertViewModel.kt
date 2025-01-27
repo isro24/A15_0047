74c7d1a85b4f0a3e5391828888506c61a4d9f23b
isro24 <alamat_email_anda@example.com>
2025-01-27 18:50:03 +0700

    Menambah validasi dan Transparan Status Bar untuk InsertViewModel, dan UpdateViewModel pada Kandang ViewModel

--- app/src/main/java/com/example/uas/ui/viewmodel/kandang/InsertViewModel.kt
+++ app/src/main/java/com/example/uas/ui/viewmodel/kandang/InsertViewModel.kt
@@ -4,15 +4,18 @@ import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.uas.model.Hewan
 import com.example.uas.model.Kandang
 import com.example.uas.repository.KandangRepository
+import com.google.accompanist.systemuicontroller.SystemUiController
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class InsertViewModelKandang(private val knd: KandangRepository): ViewModel(){
-    var uiState by mutableStateOf(InsertUiStateKandang())
+    var uiStateKandang by mutableStateOf(InsertUiStateKandang())
         private set
 
     var listHewan by mutableStateOf<List<Hewan>>(emptyList())
@@ -22,17 +25,47 @@ class InsertViewModelKandang(private val knd: KandangRepository): ViewModel(){
         fetchHewan()
     }
 
+    fun handleNavigateBack(
+        systemUiController: SystemUiController,
+        onNavigateBack: () -> Unit
+    ) {
+        systemUiController.setStatusBarColor(Color.Transparent)
+
+        onNavigateBack()
+    }
+
     fun updateInsertKndState(insertUiEventKandang: InsertUiEventKandang){
-        uiState = InsertUiStateKandang(insertUiEventKandang=insertUiEventKandang)
+        uiStateKandang = InsertUiStateKandang(insertUiEventKandang=insertUiEventKandang)
+    }
+
+    private fun hapusErrorState(){
+        viewModelScope.launch {
+            delay(5000)
+            uiStateKandang = uiStateKandang.copy(isEntryValidKandang = FormErrorStateKandang())
+        }
     }
 
     suspend fun insertKnd(){
-        viewModelScope.launch {
-            try {
-                knd.insertKandang(uiState.insertUiEventKandang.toKnd())
-            }catch (e:Exception){
-                e.printStackTrace()
+        if (validateFields()){
+            viewModelScope.launch {
+                try {
+                    knd.insertKandang(uiStateKandang.insertUiEventKandang.toKnd())
+                    uiStateKandang = InsertUiStateKandang(
+                        snackBarMessage = "Data Kandang berhasil ditambahkan",
+                        insertUiEventKandang = InsertUiEventKandang(),
+                        isEntryValidKandang = FormErrorStateKandang()
+                    )
+                }catch (e:Exception){
+                    uiStateKandang = uiStateKandang.copy(
+                        snackBarMessage = "Data gagal disimpan"
+                    )
+                }
             }
+        }
+        else{
+            uiStateKandang = uiStateKandang.copy(
+                snackBarMessage = "Input tidak valid, periksa data anda kembali"
+            )
         }
     }
 
@@ -50,10 +83,32 @@ class InsertViewModelKandang(private val knd: KandangRepository): ViewModel(){
         }
     }
 
+    fun resetSnackBarMessage() {
+        uiStateKandang = uiStateKandang.copy(snackBarMessage = null)
+    }
+
+    fun validateFields(): Boolean{
+        val event = uiStateKandang.insertUiEventKandang
+        val errorState = FormErrorStateKandang(
+            idKandangError = if (event.idKandang.isNotEmpty()) null else "Id Kandang tidak boleh kosong",
+            idHewanError = if (event.idHewan != null) null else "Id Hewan tidak boleh kosong",
+            kapasitasError = if (event.kapasitas != null) null else "Kapasitas tidak boleh kosong",
+            lokasiError = if (event.lokasi.isNotEmpty()) null else "Lokasi tidak boleh kosong",
+        )
+        uiStateKandang = uiStateKandang.copy(isEntryValidKandang = errorState)
+
+        if (!errorState.isValid()){
+            hapusErrorState()
+        }
+        return errorState.isValid()
+    }
+
 }
 
 data class InsertUiStateKandang(
-    val insertUiEventKandang: InsertUiEventKandang = InsertUiEventKandang()
+    val insertUiEventKandang: InsertUiEventKandang = InsertUiEventKandang(),
+    val isEntryValidKandang: FormErrorStateKandang = FormErrorStateKandang(),
+    val snackBarMessage: String? = null
 )
 
 data class InsertUiEventKandang(
@@ -71,7 +126,7 @@ fun InsertUiEventKandang.toKnd(): Kandang = Kandang(
 )
 
 fun Kandang.toUiStateKnd(): InsertUiStateKandang = InsertUiStateKandang(
-    insertUiEventKandang = toInsertUiEventKandang()
+    insertUiEventKandang = toInsertUiEventKandang(),
 )
 
 fun Kandang.toInsertUiEventKandang(): InsertUiEventKandang = InsertUiEventKandang(
@@ -80,4 +135,18 @@ fun Kandang.toInsertUiEventKandang(): InsertUiEventKandang = InsertUiEventKandan
     kapasitas = kapasitas,
     lokasi = lokasi
 )
+
+data class FormErrorStateKandang(
+    val idKandangError: String? = null,
+    val idHewanError: String? = null,
+    val kapasitasError: String? = null,
+    val lokasiError: String? = null,
+){
+    fun isValid(): Boolean{
+        return idKandangError == null
+                &&idHewanError ==  null
+                &&kapasitasError == null
+                &&lokasiError == null
+    }
+}
 
--- app/src/main/java/com/example/uas/ui/viewmodel/kandang/UpdateViewModel.kt
+++ app/src/main/java/com/example/uas/ui/viewmodel/kandang/UpdateViewModel.kt
@@ -3,11 +3,14 @@ package com.example.uas.ui.viewmodel.kandang
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.uas.repository.KandangRepository
 import com.example.uas.ui.view.kandang.DestinasiUpdateKandang
+import com.google.accompanist.systemuicontroller.SystemUiController
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class UpdateViewModelKandang(
@@ -18,7 +21,23 @@ class UpdateViewModelKandang(
     var updateUIStateKandang by mutableStateOf(InsertUiStateKandang())
         private set
 
+    fun handleNavigateBack(
+        systemUiController: SystemUiController,
+        onNavigateBack: () -> Unit
+    ) {
+        systemUiController.setStatusBarColor(Color.Transparent)
+
+        onNavigateBack()
+    }
+
     private val _idKandang: String = checkNotNull(savedStateHandle[DestinasiUpdateKandang.IDKANDANG])
+
+    private fun hapusErrorState(){
+        viewModelScope.launch {
+            delay(5000)
+            updateUIStateKandang = updateUIStateKandang.copy(isEntryValidKandang = FormErrorStateKandang())
+        }
+    }
 
     init {
         viewModelScope.launch {
@@ -33,12 +52,45 @@ class UpdateViewModelKandang(
     }
 
     suspend fun updateData(){
-        viewModelScope.launch {
-            try {
-                kandangRepository.updateKandang(_idKandang, updateUIStateKandang.insertUiEventKandang.toKnd())
-            }catch (e: Exception){
-                e.printStackTrace()
-            }
+       if (validateFields()){
+           viewModelScope.launch {
+               try {
+                   kandangRepository.updateKandang(_idKandang, updateUIStateKandang.insertUiEventKandang.toKnd())
+                   updateUIStateKandang = InsertUiStateKandang(
+                       snackBarMessage = "Data Kandang berhasil diubah",
+                       insertUiEventKandang = InsertUiEventKandang(),
+                       isEntryValidKandang = FormErrorStateKandang()
+                   )
+               }catch (e: Exception){
+                   updateUIStateKandang= updateUIStateKandang.copy(
+                       snackBarMessage = "Data Kandang gagal diubah"
+                   )
+               }
+           }
+       }
+        else{
+            updateUIStateKandang = updateUIStateKandang.copy(
+                snackBarMessage = "Data kandang Gagal diubah"
+            )
+       }
+    }
+
+    fun resetSnackBarMessage() {
+        updateUIStateKandang = updateUIStateKandang.copy(snackBarMessage = null)
+    }
+    fun validateFields(): Boolean{
+        val event = updateUIStateKandang.insertUiEventKandang
+        val errorState = FormErrorStateKandang(
+            idKandangError = if (event.idKandang.isNotEmpty()) null else "Id Kandang tidak boleh kosong",
+            idHewanError = if (event.idHewan != null) null else "Id Hewan tidak boleh kosong",
+            kapasitasError = if (event.kapasitas != null) null else "Kapasitas tidak boleh kosong",
+            lokasiError = if (event.lokasi.isNotEmpty()) null else "Lokasi tidak boleh kosong",
+        )
+        updateUIStateKandang = updateUIStateKandang.copy(isEntryValidKandang = errorState)
+
+        if (!errorState.isValid()){
+            hapusErrorState()
         }
+        return errorState.isValid()
     }
 }
