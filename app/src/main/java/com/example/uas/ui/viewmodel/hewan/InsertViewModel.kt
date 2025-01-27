4865b779f89e6f2971258a581939d3d196e560bb
isro24 <alamat_email_anda@example.com>
2025-01-27 18:45:32 +0700

    Membuat validasi dan Transparan Status Bar untuk InsertViewModel, dan UpdateViewModel pada Hewan ViewModel

--- app/src/main/java/com/example/uas/ui/viewmodel/hewan/InsertViewModel.kt
+++ app/src/main/java/com/example/uas/ui/viewmodel/hewan/InsertViewModel.kt
@@ -3,33 +3,88 @@ package com.example.uas.ui.viewmodel.hewan
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.uas.model.Hewan
 import com.example.uas.repository.HewanRepository
+import com.google.accompanist.systemuicontroller.SystemUiController
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class InsertViewModel(private val hwn: HewanRepository): ViewModel(){
     var uiState by mutableStateOf(InsertUiState())
         private set
 
+    fun handleNavigateBack(
+        systemUiController: SystemUiController,
+        onNavigateBack: () -> Unit
+    ) {
+        systemUiController.setStatusBarColor(Color.Transparent)
+
+        onNavigateBack()
+    }
     fun updateInsertHwnState(insertUiEvent: InsertUiEvent){
         uiState = InsertUiState(insertUiEvent=insertUiEvent)
     }
 
-    suspend fun insertHwn(){
+    private fun hapusErrorState(){
         viewModelScope.launch {
-            try {
-                hwn.insertHewan(uiState.insertUiEvent.toHwn())
-            }catch (e:Exception){
-                e.printStackTrace()
+            delay(5000)
+            uiState = uiState.copy(isEntryValid = FormErrorState())
+        }
+    }
+
+    suspend fun insertHwn(){
+        if(validateFields()){
+            viewModelScope.launch {
+                try {
+                    hwn.insertHewan(uiState.insertUiEvent.toHwn())
+                    uiState = InsertUiState(
+                        snackBarMessage = "Data Hewan berhasil ditambahkan",
+                        insertUiEvent = InsertUiEvent(),
+                        isEntryValid = FormErrorState()
+                    )
+                }catch (e:Exception){
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data gagal disimpan"
+                    )
+                }
             }
         }
+        else{
+            uiState = uiState.copy(
+                snackBarMessage = "Input tidak valid, periksa data anda kembali"
+            )
+        }
+    }
+
+    fun resetSnackBarMessage() {
+        uiState = uiState.copy(snackBarMessage = null)
+    }
+
+    fun validateFields(): Boolean{
+        val event = uiState.insertUiEvent
+        val errorState = FormErrorState(
+            namaHewanError = if(event.namaHewan.isNotEmpty()) null else "Nama Hewan tidak boleh kosong" ,
+            tipePakanError = if(event.tipePakan.isNotEmpty()) null else "Tipe Pakan tidak boleh kosong",
+            populasiError = if(event.populasi != null) null else "Populasi tidak boleh kosong",
+            zonaWilayahError = if(event.zonaWilayah.isNotEmpty()) null else "Zona Wilayah tidak boleh kosong"
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+
+        if (!errorState.isValid()){
+            hapusErrorState()
+        }
+        return errorState.isValid()
     }
 }
 
 data class InsertUiState(
-    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+    val insertUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null
+
 )
 
 data class InsertUiEvent(
@@ -59,4 +114,19 @@ fun Hewan.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
     populasi=populasi,
     zonaWilayah=zonaWilayah
 )
+
+data class FormErrorState(
+    val namaHewanError:String?=null,
+    val tipePakanError:String?=null,
+    val populasiError:String?=null,
+    val zonaWilayahError:String?=null
+){
+    fun isValid(): Boolean{
+        return namaHewanError == null
+                && tipePakanError == null
+                && populasiError == null
+                && zonaWilayahError == null
+
+    }
+}
 
--- app/src/main/java/com/example/uas/ui/viewmodel/hewan/UpdateViewModel.kt
+++ app/src/main/java/com/example/uas/ui/viewmodel/hewan/UpdateViewModel.kt
@@ -3,11 +3,14 @@ package com.example.uas.ui.viewmodel.hewan
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.uas.repository.HewanRepository
 import com.example.uas.ui.view.hewan.DestinasiUpdateHewan
+import com.google.accompanist.systemuicontroller.SystemUiController
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class UpdateViewModel(
@@ -18,7 +21,23 @@ class UpdateViewModel(
     var updateUIState by mutableStateOf(InsertUiState())
         private set
 
+    fun handleNavigateBack(
+        systemUiController: SystemUiController,
+        onNavigateBack: () -> Unit
+    ) {
+        systemUiController.setStatusBarColor(Color.Transparent)
+
+        onNavigateBack()
+    }
+
     private val _idHewan: String = checkNotNull(savedStateHandle[DestinasiUpdateHewan.IDHEWAN])
+
+    private fun hapusErrorState(){
+        viewModelScope.launch {
+            delay(5000)
+            updateUIState = updateUIState.copy(isEntryValid = FormErrorState())
+        }
+    }
 
     init {
         viewModelScope.launch {
@@ -33,12 +52,45 @@ class UpdateViewModel(
     }
 
     suspend fun updateData(){
-        viewModelScope.launch {
-            try {
-                hewanRepository.updateHewan(_idHewan, updateUIState.insertUiEvent.toHwn())
-            }catch (e: Exception){
-                e.printStackTrace()
+        if (validateFields()){
+            viewModelScope.launch {
+                try {
+                    hewanRepository.updateHewan(_idHewan, updateUIState.insertUiEvent.toHwn())
+                    updateUIState = InsertUiState(
+                        snackBarMessage = "Data Hewan berhasil diubah",
+                        insertUiEvent = InsertUiEvent(),
+                        isEntryValid = FormErrorState()
+                    )
+                }catch (e: Exception){
+                    updateUIState = updateUIState.copy(
+                        snackBarMessage = "Data Hewan gagal diubah"
+                    )
+                }
             }
         }
+        else{
+            updateUIState = updateUIState.copy(
+                snackBarMessage = "Data Hewan gagal diubah"
+            )
+        }
+    }
+    fun resetSnackBarMessage() {
+        updateUIState = updateUIState.copy(snackBarMessage = null)
+    }
+
+    fun validateFields(): Boolean{
+        val event = updateUIState.insertUiEvent
+        val errorState = FormErrorState(
+            namaHewanError = if(event.namaHewan.isNotEmpty()) null else "Nama Hewan tidak boleh kosong" ,
+            tipePakanError = if(event.tipePakan.isNotEmpty()) null else "Tipe Pakan tidak boleh kosong",
+            populasiError = if(event.populasi != null) null else "Populasi tidak boleh kosong",
+            zonaWilayahError = if(event.zonaWilayah.isNotEmpty()) null else "Zona Wilayah tidak boleh kosong"
+        )
+        updateUIState = updateUIState.copy(isEntryValid = errorState)
+
+        if (!errorState.isValid()){
+            hapusErrorState()
+        }
+        return errorState.isValid()
     }
 }
